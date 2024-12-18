c34cf10116924bbaaaeb0c23f374e36c0f24ea2f
Alexander01998 <contact.wurstimperium@gmail.com>
2024-12-18 18:20:03 +0100

    Try custom jekyll plugin for anchor links
    I have no idea if this will work.

---
+++ _plugins/heading_anchors.rb
@@ -0,0 +1,35 @@
+module Jekyll
+  class HeadingAnchorsGenerator < Generator
+    safe true
+    priority :low
+
+    def generate(site)
+      site.pages.each { |page| process_content(page) }
+      site.posts.docs.each { |post| process_content(post) }
+    end
+
+    private
+
+    def process_content(item)
+      return unless item.output?
+      return if item.output.nil?
+
+      doc = Nokogiri::HTML::DocumentFragment.parse(item.output)
+      
+      doc.css('h1, h2, h3, h4, h5, h6').each do |heading|
+        next unless heading['id']
+        
+        # Create anchor element
+        anchor = Nokogiri::XML::Node.new('a', doc)
+        anchor['href'] = "##{heading['id']}"
+        anchor['class'] = 'heading-anchor'
+        anchor.content = '🔗'
+        
+        # Insert anchor before heading content
+        heading.prepend_child(anchor)
+      end
+
+      item.output = doc.to_html
+    end
+  end
+end 
